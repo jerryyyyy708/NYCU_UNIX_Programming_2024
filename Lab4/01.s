dc5312c819d42362ecd405508bdebf877fa21800
jerryyyyy708 <jerryyyyy708@gmail.com>
2024-05-26 00:00:14 +0800

    Discard Duplicate

--- Lab4/01.s
+++
@@ -1,6 +0,0 @@
-mov eax, [0x600000]
-add eax, [0x600004]
-sub eax, [0x600008]
-mov [0x60000c], eax
-done:
-ASM{b30ea3278ec8e1eb79f9931ebf572890df2befeb}
--- Lab4/03.s
+++
@@ -1,4 +0,0 @@
-mov ebx, 0xFFFDFFFF
-and eax, ebx
-done:
-ASM{b5fe87b2c36a2cfd45bf7fe4a8311752e50d3115}
